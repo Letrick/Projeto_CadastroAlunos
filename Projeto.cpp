33d89f5489398ade68a71729a63133ac1d1a61a0
Patrick Leal <80593781+Letrick@users.noreply.github.com>
2021-12-01 17:57:34 -0300

    Add files via upload

---
+++ Projeto.cpp
@@ -0,0 +1,30 @@
+#include "funcoes.cpp"
+
+
+int main () {
+char opcao = ' ';
+do{
+	 opcao = menu();
+
+	switch (opcao) {
+			case '1':
+			funcaoCadastro ();
+			break;
+		
+			case '2':
+				
+			std::cout << "Gerar relatorio \n";
+
+			break;
+			
+			case '3':
+			std::cout << "\nFinalizando sessao\n";
+			
+			break;
+			default:
+	   		std::cout << "\nOpcao invalida\n Tente Novamente!\n";
+			}
+		
+	}while (opcao != '3');
+ 
+}
